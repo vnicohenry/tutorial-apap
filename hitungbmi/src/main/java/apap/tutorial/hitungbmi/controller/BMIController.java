0b931fa2d4c6471d0aba44282a38e4ff09448c03
Nicolas Henry <valentinusnicx@gmail.com>
2020-09-24 16:33:02 +0700

    feat: tutorial 1 - initialize spring boot project v2

--- hitungbmi/src/main/java/apap/tutorial/hitungbmi/controller/BMIController.java
+++ hitungbmi/src/main/java/apap/tutorial/hitungbmi/controller/BMIController.java
@@ -1,7 +1,7 @@
 package apap.tutorial.hitungbmi.controller;
 
 import apap.tutorial.hitungbmi.model.BMI;
-import org.springframework.stereotype.Controller ;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
